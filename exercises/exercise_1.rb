496fc8e0aba6139a492c97c609db7dbae444dc22
promilo <milind.shah@mail.utoronto.ca>
2017-06-06 16:48:35 +0000

    finished ex 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,8 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: TRUE, womens_apparel: TRUE)
+Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: FALSE, womens_apparel: TRUE)
+Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: TRUE, womens_apparel: FALSE)
+
+puts "Number of stores are #{Store.count}"
