c0564deb0a3be8ccf2d78b3139e7d49e84416e49
promilo <milind.shah@mail.utoronto.ca>
2017-06-06 17:27:37 +0000

    finished ex5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@sumMoney = Store.sum(:annual_revenue)
+puts "Total money is #{@sumMoney}"
+@average = @sumMoney / Store.count
+puts "the average is #{@average}"
+
+@rich = Store.where('stores.annual_revenue <= ?', 1000000).count
+puts "the number of rich stores are #{@rich}"
