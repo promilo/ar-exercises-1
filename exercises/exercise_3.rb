a7ea8ca9eeba974a4a41dfebe20db5be4f57796a
promilo <milind.shah@mail.utoronto.ca>
2017-06-06 17:03:02 +0000

    Finished ex 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,7 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find(3)
+p @store3
+Store.destroy(3)
+puts "Number of stores are #{Store.count} and it should be 2 coz you deleted one."
